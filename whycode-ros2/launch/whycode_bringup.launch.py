850d9efa5bcdce925e47ee4c34d505582ae2e343
jforjoe <joelsiby2003@gmail.com>
2026-08-04 13:25:51 +0530

    fix: fixed calibration file location

--- whycode-ros2/launch/whycode_bringup.launch.py
+++ whycode-ros2/launch/whycode_bringup.launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
                 'camera_frame_id': 'usb_cam',
                 'av_device_format': 'YUV422P',
                 'camera_name': 'narrow_stereo',
-                'camera_info_url': 'file:///home/joe/.ros/camera_info/camera_info.yaml',
+                'camera_info_url': 'file://$(env HOME)/.ros/camera_info/camera_info.yaml',
             }]
         ),
 
